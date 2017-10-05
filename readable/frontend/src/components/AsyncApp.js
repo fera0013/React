17db2c715b8888219659874ea91681f9f3a720ca
fera0013 <ralphfehrer@gmail.com>
2017-10-05 18:33:03 +0200

    code moved to posts component and routing fixed

--- readable/frontend/src/components/AsyncApp.js
+++ readable/frontend/src/components/AsyncApp.js
@@ -2,79 +2,22 @@
  * Created by z0017fjy on 26.09.2017.
  */
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import {
-    selectCategory,
-    fetchPostsIfNeeded,
-    invalidateCategory
-} from '../actions/post'
-import Picker from '../components/Picker'
 import Posts from '../components/Posts'
-import * as ReadableAPI from "../utils/ReadableAPI";
-import {Link, Route} from "react-router-dom";
-import {Post} from "./Post";
+import {Route} from "react-router-dom";
 
 
 
 
 class AsyncApp extends Component {
-    state={
-        categories:['all'],
-        sortMethods:['voteScore','timestamp'],
-        sortMethod: 'voteScore'
-    }
-    constructor(props) {
-        super(props)
-        this.handleChange = this.handleChange.bind(this)
-    }
-
-    componentDidMount() {
-        const { dispatch, selectedCategory,posts } = this.props
-        dispatch(fetchPostsIfNeeded(selectedCategory))
-        ReadableAPI.getAllCategories().then((categories) => {
-            this.setState({ categories: [...this.state.categories, ...categories ] })
-        })
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.selectedCategory !== prevProps.selectedCategory) {
-            const { dispatch, selectedCategory } = this.props
-            dispatch(fetchPostsIfNeeded(selectedCategory))
-        }
-    }
-
-    handleChange(nextCategory) {
-        this.props.dispatch(selectCategory(nextCategory))
-        this.props.dispatch(fetchPostsIfNeeded(nextCategory))
-    }
-
-
     render() {
-        const { selectedCategory, posts } = this.props
         return (
             <div>
-                {this.state.categories.map((category) => (
-                    <Link  to={`/${category}`}>{category}</Link>
-                ))}
-                <Route path='/:category' render={() => (
+                <Route  path='/' render={() => (
+                    <Posts/>
+                )}/>
+                <Route exact path='/:create' render={() => (
                     <div>
-                        <Picker
-                            value={'Category: '+selectedCategory}
-                            onChange={this.handleChange}
-                            options={this.state.categories}
-                        />
-                        <Picker
-                            value={'Sort by: '+this.state.sortMethod}
-                            options={this.state.sortMethods}
-                            onChange={(itemValue, itemIndex) => this.setState({sortMethod: itemValue})}
-                        />
-                        {posts.length > 0 &&
-                        <Posts posts={posts.filter((post)=>{
-                            return 'all' === this.props.selectedCategory || this.props.selectedCategory === post.category
-                        }).sort((post_1,post_2)=>{
-                            return post_1[this.state.sortMethod]<post_2[this.state.sortMethod]
-                        })} />}
+                       Create
                     </div>
                 )}/>
             </div>
@@ -82,31 +25,5 @@ class AsyncApp extends Component {
     }
 }
 
-AsyncApp.propTypes = {
-    selectedCategory: PropTypes.string.isRequired,
-    posts: PropTypes.array.isRequired,
-    isFetching: PropTypes.bool.isRequired,
-    lastUpdated: PropTypes.number,
-    dispatch: PropTypes.func.isRequired
-}
 
-function mapStateToProps(state) {
-    const { selectedCategory, postsByCategory } = state
-    const {
-        isFetching,
-        lastUpdated,
-        items: posts
-    } = postsByCategory[selectedCategory] || {
-        isFetching: true,
-        items: []
-    }
-
-    return {
-        selectedCategory,
-        posts,
-        isFetching,
-        lastUpdated
-    }
-}
-
-export default connect(mapStateToProps)(AsyncApp)
+export default AsyncApp
--- readable/frontend/src/components/Posts.js
+++ readable/frontend/src/components/Posts.js
@@ -4,22 +4,100 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Post from './Post'
+import * as ReadableAPI from "../utils/ReadableAPI";
+import {
+    selectCategory,
+    fetchPostsIfNeeded,
+    invalidateCategory
+} from '../actions/post'
+import {Link} from "react-router-dom";
+import Picker from "./Picker";
+import {dispatch} from "redux";
+import {connect} from "react-redux";
 
-export default class Posts extends Component {
+export class Posts extends Component {
+    state={
+        sortMethods:['voteScore','timestamp'],
+        sortMethod: 'voteScore',
+        categories:['all']
+    }
+    constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    componentDidMount() {
+        const { selectedCategory } = this.props
+        ReadableAPI.getAllCategories().then((categories) => {
+            this.setState({ categories: [...this.state.categories, ...categories ] })})
+        this.props.dispatch(fetchPostsIfNeeded(selectedCategory))
+    }
+    handleChange(nextCategory) {
+        this.props.dispatch(selectCategory(nextCategory))
+        this.props.dispatch(fetchPostsIfNeeded(nextCategory))
+    }
+
     render() {
+        const { posts } = this.props
         return (
             <div>
+                {this.state.categories.map((category) => (
+                    <Link
+                        to={`/${category}`}
+                        onClick={() => this.handleChange(category)}
+                        key={category}>{category}
+                    </Link>
+                ))}
+                <Picker
+                    value={'Sort by: '+this.state.sortMethod}
+                    options={this.state.sortMethods}
+                    onChange={(itemValue, itemIndex) => this.setState({sortMethod: itemValue})}
+                />
                 <ul>
-                    {this.props.posts.map((post, i) => <li key={i}>{post.title}</li>)}
+                    {posts.filter((post)=>{
+                        return 'all' === this.props.selectedCategory || this.props.selectedCategory === post.category
+                    }).sort((post_1,post_2)=>{
+                        return post_1[this.state.sortMethod]<post_2[this.state.sortMethod]
+                    }).map((post, i) => <li key={i}>{post.title}</li>)}
                 </ul>
                 {this.props.posts.length > 0 &&
                     <Post post_id={this.props.posts[0].id}/>
                 }
+                <div className="open-search">
+                    <Link
+                        to='/create'
+                        className='add-contact'
+                    >Add a post</Link>
+                </div>
             </div>
         )
     }
 }
 
 Posts.propTypes = {
-    posts: PropTypes.array.isRequired
+    selectedCategory: PropTypes.string.isRequired,
+    posts: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    lastUpdated: PropTypes.number,
+    dispatch: PropTypes.func.isRequired
 }
+
+function mapStateToProps(state) {
+    const { selectedCategory, postsByCategory } = state
+    const {
+        isFetching,
+        lastUpdated,
+        items: posts
+    } = postsByCategory[selectedCategory] || {
+        isFetching: true,
+        items: []
+    }
+
+    return {
+        selectedCategory,
+        posts,
+        isFetching,
+        lastUpdated
+    }
+}
+
+export default connect(mapStateToProps)(Posts)
